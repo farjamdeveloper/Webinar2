6a4811bc6400431e844cb7b97b3e193a0a3d7d68
farjam mahdavi <eng.mahdavi@live.com>
2020-05-08 16:24:57 +0430

    Add project files.

---
+++ Webinar2/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Webinar2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int number1 = Convert.ToInt32(textBox1.Text);
+            int number2 = Convert.ToInt32(textBox2.Text);
+            int sum=number1 + number2;
+            textBox3.Text = sum.ToString();
+        }
+    }
+}
